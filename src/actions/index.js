d338ff0bcd9979e9beed8bc813cf5981bf35ebc5
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-03-20 16:27:50 -0400

    created reducers and fixed syntax error

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
 
-export const FETCH_CHARACTER_START = 'FETCH_CHARACTER_START';
+export const FETCH_CHARACTER_FETCHING = 'FETCH_CHARACTER_FETCHING';
 export const FETCH_CHARACTER_SUCCESS = 'FETCH_CHARACTER_SUCCESS';
 export const FETCH_CHARACTER_FAILURE = 'FETCH_CHARACTER_FAILURE';
 
@@ -14,15 +14,15 @@ export const FETCH_CHARACTER_FAILURE = 'FETCH_CHARACTER_FAILURE';
 
 
 export const getCharacter = () => dispatch => {
-    dispatch({ type: FETCH_CHARACTER_START});
+    dispatch({ type: FETCH_CHARACTER_FETCHING});
     axios
         .get(
             'https://swapi.co/api/people'
         )
         .then(response => 
-            dispatch({ type: FETCH_PHOTO_SUCCESS, payload: response.data})
+            dispatch({ type: FETCH_CHARACTER_SUCCESS, payload: response.data})
             )
         .catch(error =>
-            dispatch({ type: FETCH_PHOTO_FAILURE, payload: error})
+            dispatch({ type: FETCH_CHARACTER_FAILURE, payload: error})
             )
-}
+};
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,40 @@
-import /* we need our action types here*/ "../actions";
+import {
+  FETCH_CHARACTER_FETCHING, 
+  FETCH_CHARACTER_SUCCESS,
+  FETCH_CHARACTER_FAILURE} from "../actions";
+
+
 const initialState = {
-  characters: []
+  characters: [],
+  boolean: true,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
+
+
 export const charsReducer = (state = initialState, action) => {
+  console.log('charsreducer', action);
   switch (action.type) {
+    case FETCH_CHARACTER_FETCHING:
+    return {
+      ...state,
+      error: null,
+      boolean: true
+    };
+    case FETCH_CHARACTER_SUCCESS:
+    return {
+      ...state,
+      characters: action.payload,
+      boolean: true,
+      error: null
+    };
+    case FETCH_CHARACTER_FAILURE:
+    return {
+      ...state,
+      error: action.payload,
+      boolean: false
+    }
+
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
@@ -12,3 +42,5 @@ export const charsReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+export default charsReducer;
